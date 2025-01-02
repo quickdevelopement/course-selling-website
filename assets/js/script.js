c914c27ede1e537dd09cf38409ee755fa2d75992
ariful4082 <ariful4082@gmail.com>
2025-01-02 23:15:24 +0600

    course selling website desing: Menu Design with responsive

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+const mobileMenu = document.getElementById("mobileMenu");
+
+const navigation = document.getElementById("navigtion");
+
+mobileMenu.addEventListener("click", function () {
+//   console.log('click')
+  navigation.classList.toggle("toggle");
+});
