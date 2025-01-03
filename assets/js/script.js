1d11ce296f6109b67182852299f51badc1aedc9e
ariful4082 <ariful4082@gmail.com>
2025-01-03 12:25:39 +0600

    Slide included this project

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,30 @@ mobileMenu.addEventListener("click", function () {
 //   console.log('click')
   navigation.classList.toggle("toggle");
 });
+
+
+// Slider
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  // margin:10,
+  responsiveClass:true,
+  autoplay:true,
+  dots: true,
+  autoplayTimeout:5000,
+  navText: [`<i class="fa-solid fa-chevron-left"></i>`,`<i class="fa-solid fa-chevron-right"></i>`],
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:1,
+          nav:false
+      },
+      1000:{
+          items:1,
+          nav:true
+      }
+  }
+})
