d9302b278ae92f585abcbdc8fd50bf210728df89
ariful4082 <ariful4082@gmail.com>
2025-02-04 07:55:58 +0600

    complate tastimonial section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,72 @@
-
 const mobileMenu = document.getElementById("mobileMenu");
 
 const navigation = document.getElementById("navigtion");
 
 mobileMenu.addEventListener("click", function () {
-//   console.log('click')
+  //   console.log('click')
   navigation.classList.toggle("toggle");
 });
 
-
 // Slider
 
-$('.owl-carousel').owlCarousel({
-  loop:true,
+$(".hero-carousel").owlCarousel({
+  loop: true,
   // margin:10,
-  responsiveClass:true,
-  autoplay:true,
+  responsiveClass: true,
+  autoplay: true,
   dots: true,
-  autoplayTimeout:5000,
-  navText: [`<i class="fa-solid fa-chevron-left"></i>`,`<i class="fa-solid fa-chevron-right"></i>`],
-  responsive:{
-      0:{
-          items:1,
-          nav:true
+  autoplayTimeout: 5000,
+  navText: [
+    `<i class="fa-solid fa-chevron-left"></i>`,
+    `<i class="fa-solid fa-chevron-right"></i>`,
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 1,
+      nav: true,
+    },
+  },
+});
+
+$(document).ready(function () {
+  $(".testimonial-carousel").owlCarousel({
+    dots: true,
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        dotsEach: true,
       },
-      600:{
-          items:1,
-          nav:false
+      600: {
+        items: 1,
+        dotsEach: true,
       },
-      1000:{
-          items:1,
-          nav:true
-      }
-  }
-})
+      768: {
+        items: 2,
+        dotsEach: true
+      },
+
+      992: {
+        items: 3,
+        dotsEach: true
+      },
+      1200: {
+        items: 4,
+        dotsEach: true
+      },
+    },
+  });
+});
